c55cd1eae1e809b21c61d48e3775446a5fb1c614
Diana Koeswanto <d.koeswanto@gmail.com>
2024-08-21 22:47:17 +1000

    Changed to require instead of require_relative

--- spec/command/place_command_spec.rb
+++ spec/command/place_command_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require_relative '../spec_helper'
-require_relative '../../lib/command/place_command'
-require_relative '../../lib/model/robot'
+require "spec_helper"
+require "command/place_command"
+require "model/robot"
 
 describe PlaceCommand do
   let(:robot) { instance_double(Robot) }
